dccf17416047592675698e6c6a6f2a7483eb891e
unknown <kmrinandita@gmail.com>
2023-06-03 20:30:06 +0530

    first forge

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Chat from "./components/Chat/Chat";
+import Login from "./components/Login/Login";
+import { useStateValue } from "./components/Utility/StateProvider";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  const [{ user }, dispatch] = useStateValue();
+
+  return (
+    <div className="App">
+      <Router>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <div className="app_body">
+              <Sidebar />
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <h1>Welcome to #FORGE</h1>
+                </Route>
+              </Switch>
+            </div>
+          </>
+        )}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import "./Sidebar.css";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import CreateIcon from "@material-ui/icons/Create";
+import SidebarOption from "./SidebarOption";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+import db from "../Firebase/firebase";
+import { useStateValue } from "../Utility/StateProvider";
+
+function Sidebar() {
+  const [channels, setChannels] = useState([]);
+  const [{ user }] = useStateValue();
+
+  useEffect(() => {
+    // Run this code ONCE when the sidebar component loads
+    db.collection("rooms").onSnapshot((snapshot) =>
+      setChannels(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          name: doc.data().name,
+        }))
+      )
+    );
+  }, []);
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar_header">
+        <div className="sidebar_info">
+          <h2>{user?.displayName}</h2>
+          <h3>
+            <FiberManualRecordIcon />
+            {user?.displayName}
+          </h3>
+        </div>
+        <CreateIcon />
+      </div>
+      <SidebarOption Icon={InsertCommentIcon} title={"Thread"} />
+      <SidebarOption Icon={InboxIcon} title={"Mentions & reactions"} />
+      <SidebarOption Icon={DraftsIcon} title={"Saved items"} />
+      <SidebarOption Icon={BookmarkBorderIcon} title={"Channel browser"} />
+      <SidebarOption Icon={PeopleAltIcon} title={"People & user groups"} />
+      <SidebarOption Icon={AppsIcon} title={"Apps"} />
+      <SidebarOption Icon={FileCopyIcon} title={"File browser"} />
+      <SidebarOption Icon={ExpandLessIcon} title={"Show less"} />
+      <hr />
+      <SidebarOption Icon={ExpandMoreIcon} title={"Channels"} />
+      <hr />
+      <SidebarOption Icon={AddIcon} title={"Add Channel"} addChannelOption />
+      {/* Connect db and list all the channels */}
+      {channels.map((channel) => (
+        <SidebarOption title={channel.name} id={channel.id} />
+      ))}
+      {/* SidebarOptions... */}
+    </div>
+  );
+}
+
+export default Sidebar;
